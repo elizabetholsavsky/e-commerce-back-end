16ff298f0641967239e028e993509e79977bc69f
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-06 15:53:16 -0500

    move/revise comments in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories, include its associated Products
 router.get('/', async (req, res) => {
-  // find all categories, include its associated Products
   try {
     const categoriesData = await Category.findAll({
       include: [{model: Product}]
@@ -22,8 +22,8 @@ router.get('/', async (req, res) => {
   };
 });
 
+// find one category by its `id` value, include its associated Products
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value, include its associated Products
   try {
     const categoryById = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -41,8 +41,8 @@ router.get('/:id', async (req, res) => {
   };
 });
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const newCategory = await Category.create({
       category_name: req.body.categoryName
@@ -56,8 +56,8 @@ router.post('/', async (req, res) => {
   };
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
     const updateCategory = await Category.update({
       category_name: req.body.categoryName
@@ -85,8 +85,8 @@ router.put('/:id', async (req, res) => {
   };
 });
 
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
     const deleteCategory = await Category.destroy(
       {
