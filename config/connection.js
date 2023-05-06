5b734108f666f103a9c4285ed4de69ea73e9b83d
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-06 11:56:54 -0500

    set up associations for Sequelize models

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// JawsDB, use for deployment to Heroku
+// https://devcenter.heroku.com/articles/jawsdb#:~:text=JawsDB%20is%20a%20MySQL%2FMariaDB,for%20use%20with%20your%20application.
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
