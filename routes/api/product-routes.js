9bbcf774e2e7c0b573c8f1f92218c0c745efef06
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-06 16:10:14 -0500

    add get product route, tested in insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// find all products, include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag}],
+    });
+    if (!productData) {
+      res.status(200).json({message: 'No products found'});
+      return;
+    }
+
+    res.json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// find a single product by its `id`, include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {} catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -89,8 +102,11 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {} catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
