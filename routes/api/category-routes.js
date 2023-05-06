7a1988e027aef938eb5cac47c80a799f4abd6370
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-06 13:11:53 -0500

    outline async try/catch for routes, add catch err 500

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // find all categories, include its associated Products
+  try {} catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value, include its associated Products
+  try {} catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {} catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {} catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {} catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
